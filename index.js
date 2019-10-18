e47f9335f6a7ded33d82cf0e671ba9a0fce3d498
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-10-18 16:30:15 -0300

    versopm 1.2.2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let SimpleAlerts = require('./SimpleAlerts.js');
 let SimpleConfirms = require('./SimpleConfirms.js');
+let SimpleFormAlerts = require('./SimpleFormAlerts.js');
 module.exports = {
     "SimpleAlerts": SimpleAlerts,
     "SimpleConfirms": SimpleConfirms,
+    "SimpleFormAlerts": SimpleFormAlerts,
 }
